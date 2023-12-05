f5ab05aede08817881ed8b467376140527d2a2b9
lucifer1708 <hello@sumitdhiman.in>
2023-12-05 23:02:02 +0530

    added jazzmin panel

--- shortner/settings.py
+++ shortner/settings.py
@@ -21,16 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-0(isy4%6=*gyje!2xg8svf!hb*10rc+hurvuifg0*b7$@s$r8u"
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
-    "localhost",
-    "http://short.sumitdhiman.in",
+    "urls.istenith.com",
     "sumitdhiman.in",
-    "https://short.sumitdhiman.in",
 ]
 
 
